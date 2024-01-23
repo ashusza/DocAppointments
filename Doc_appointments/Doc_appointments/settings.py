b18eb37532c2077a6824881f428358527c7a5028
ashusza <asijapati58@gmail.com>
2024-01-23 17:41:51 +0545

    Database mysql

--- Doc_appointments/Doc_appointments/settings.py
+++ Doc_appointments/Doc_appointments/settings.py
@@ -82,11 +82,14 @@ WSGI_APPLICATION = 'Doc_appointments.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',  #db name
+        'USER': 'root',
+        'PASSWORD': 'root123',
+        'HOST': 'localhost',  # or the hostname where your MySQL server is running
+        'PORT': '3306',      # or the port on which your MySQL server is listening
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
