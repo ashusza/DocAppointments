a6af428af48c14e46867882cab1e810fc55e47ef
ashusza <asijapati58@gmail.com>
2024-01-23 12:59:33 +0545

    Reupdate

--- Doc_appointments/Doc_appointments/settings.py
+++ Doc_appointments/Doc_appointments/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os 
+from django.contrib import messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,11 +123,21 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static' )]
+
+#media configuration
+MEDIA_URL="/media/"
+MEDIA_ROOT=os.path.join(BASE_DIR,",media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#message framework attributes
+MESSAGE_TAGS={
+    messages.ERROR:"danger"
+}
 
 # Changes made 
 AUTHENTICATION_BACKENDS = ['app_accounts.authentication.EmailBackend', 'django.contrib.auth.backends.ModelBackend']
